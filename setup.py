85bb94471cf6e20f21d288d43e7ad9836a48d54f
Jorden Kreps <jakereps@users.noreply.github.com>
2016-10-19 15:58:51 -0700

    FIX: Importing for __version__ broke install (#4)

--- setup.py
+++ setup.py
@@ -6,13 +6,22 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import re
+import ast
+
 from setuptools import setup, find_packages
 
-import q2templates
+# version parsing from __init__ pulled from Flask's setup.py
+# https://github.com/mitsuhiko/flask/blob/master/setup.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('q2templates/__init__.py', 'rb') as f:
+    hit = _version_re.search(f.read().decode('utf-8')).group(1)
+    version = str(ast.literal_eval(hit))
 
 setup(
     name="q2templates",
-    version=q2templates.__version__,
+    version=version,
     license='BSD-3-Clause',
     packages=find_packages(exclude=['tests']),
     package_data={
