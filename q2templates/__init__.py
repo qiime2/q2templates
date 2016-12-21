593c163db0f088520fde9df56be355757d1b17cc
Matthew Dillon <matthewrdillon@gmail.com>
2016-12-21 10:19:18 -0700

    ENH: new date-based versioning; `qiime` -> `qiime2` package rename
    New date-based versioning scheme is:
    
    - YYYY.M.patch.dev0 for development version (e.g. 2017.2.0.dev0, 2017.2.1.dev0, 2018.10.0.dev0)
    - YYYY.M.patch for release version (e.g. 2017.2.0, 2017.2.1, 2018.10.0)
    
    Plugins/interfaces are recommended to specify `qiime2 == YYYY.M.*` in setup.py's `install_requires` to obtain patch releases for a given train release.
    
    The new versioning scheme is PEP 440 compliant.

--- q2templates/__init__.py
+++ q2templates/__init__.py
@@ -1,13 +1,16 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME development team.
+# Copyright (c) 2016-2017, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
-# The full license is in the file COPYING.txt, distributed with this software.
+# The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+
+import pkg_resources
 
 from ._templates import render
 
-__version__ = '0.0.7.dev0'
+
+__version__ = pkg_resources.get_distribution('q2templates').version
 
 __all__ = ['render']
