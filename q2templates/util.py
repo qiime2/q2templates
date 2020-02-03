908744e3fed04ada1b6c225db681197e5267e785
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-02-03 15:14:00 -0700

    Allow for compatibility for pandas>=0.25.3

--- q2templates/util.py
+++ q2templates/util.py
@@ -42,8 +42,13 @@ def df_to_html(df, border=0, classes=('table', 'table-striped', 'table-hover'),
     .. [2] https://github.com/pandas-dev/pandas/issues/1852
 
     """
-    with pd.option_context('display.max_colwidth', -1):
-        return df.to_html(border=border, classes=classes, **kwargs)
+    try:
+        with pd.option_context('display.max_colwidth', -1):
+            return df.to_html(border=border, classes=classes, **kwargs)
+    except ValueError:
+        # Supports pandas API changes starting in 1.0
+        with pd.option_context('display.max_colwidth', None):
+            return df.to_html(border=border, classes=classes, **kwargs)
 
 
 def copy_assets(source_dir, output_dir):
